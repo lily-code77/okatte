cd46edd23c21b2aa9124972cd6242b8550ee0622
totoki <totoki.sayoko@gmail.com>
2024-06-21 22:01:30 +0900

    in the middle of styling

--- resources/views/myPage.blade.php
+++ resources/views/myPage.blade.php
@@ -1,83 +1,70 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('マイページ') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("記事一覧") }}
-                    @foreach($myArticles as $myArticle)
-                        @if ($myArticle['status'] == 'publish')
-                            <a href="{{ route('article.show', ['article' => $myArticle->id]) }}">
-                                <tr>
-                                    <div class="q_wrapper">
-                                        <td>
-                                            <div class="q_list">
-                                                <img class=q_img src="{{ asset('storage/' . $myArticle->image) }}" alt="">
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="q_list_content">
-                                                <p class="q_list_title">{{ $myArticle->title }}</p>
-                                                <p class="q_list_tags">{{ $myArticle->tags }}</p>
-                                                <p class="q_list_updated">{{ $myArticle->updated_at }}</p>
-                                            </div>
-                                        </td>
-                                    </div>
-                                    <div>
-                                        <div>
+    <div class="">
+        <div class="container m-8">
+            <p class="tcl text-4xl font-extrabold">{{ __("記事一覧") }}</p>
+                <div class="container px-5 py-10 mx-auto">
+                    <div class="flex justify-around flex-wrap -m-4">
+                        @foreach($myArticles as $myArticle)
+                            @if ($myArticle['status'] == 'publish')
+                                <div class="p-4 m-4 md:w-1/3 rounded border bg-white flex-1">
+                                    <a href="{{ route('article.show', ['article' => $myArticle->id]) }}">
+                                        <div class="">
+                                            <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="{{ asset('storage/' . $myArticle->image) }}" alt="">
+                                        </div>
+                                        <div class="">
+                                            <p class="text-2xl font-bold text-pretty tcl">{{ $myArticle->title }}</p>
+                                            <p class="text-base text-slate-500">{{ $myArticle->tags }}</p>
+                                            <p class="text-xs text-slate-400">{{ $myArticle->updated_at }}</p>
+                                        </div>
+                                        <div class="flex mt-3">
                                             <form action="{{ route('article.edit', ['article' => $myArticle->id]) }}" method="GET">
                                             @csrf
-
-                                                <button type="submit"  class="btn bg-blue-500 rounded-lg">
+                                                <button type="submit"  class="focus:outline-none button focus:ring-blue-500/50 font-medium rounded text-sm px-3 py-2 ml-2">
                                                     更新
                                                 </button>
                                             </form>
-                                        </div>
-                                        <div>
                                             <form action="{{ route('article.destroy', ['article' => $myArticle->id]) }}" method="POST">
                                                 @csrf
                                                 @method('DELETE')
-                        
-                                                <button type="submit"  class="btn bg-blue-500 rounded-lg">
+                                                <button type="submit"  class="focus:outline-none button focus:ring-blue-500/50 font-medium rounded text-sm px-3 py-2 ml-2">
                                                     削除
                                                 </button>
                                             </form>
                                         </div>
-                                    </div>
-                                </tr>
-                            </a>
-                        @endif
-                    @endforeach
+                                            
+                                        
+                                    </a>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                    
+                    </div>
+                    
                     <!-- ページャー -->
-                    {{ $myArticles->links() }}
-                </div>
-            </div>
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div>
+                        {{ $myArticles->links() }}
+                    </div>
+        </div>
+        <div class="">
+                <div class="">
                     {{ __("レシピ一覧") }}
                     @foreach($myRecipes as $myRecipe)
                         @if ($myRecipe['status'] == 'publish')
                             <a href="{{ route('recipe.show', ['recipe' => $myRecipe->id]) }}">
-                                <tr>
                                     <div class="q_wrapper">
-                                        <td>
                                             <div class="q_list">
                                                 <img class=q_img src="{{ asset('storage/' . $myRecipe->image) }}" alt="">
                                             </div>
-                                        </td>
-                                        <td>
+                                        
                                             <div class="q_list_content">
                                                 <p class="q_list_title">{{ $myRecipe->title }}</p>
                                                 <p class="q_list_tags">{{ $myRecipe->tags }}</p>
                                                 <p class="q_list_intro">{{ $myRecipe->intro }}</p>
                                                 <p class="q_list_updated">{{ $myRecipe->updated_at }}</p>
                                             </div>
-                                        </td>
+                                        
                                     </div>
                                     <div>
                                         <div>
@@ -106,14 +93,13 @@
                                             </form>
                                         </div>
                                     </div>
-                                </tr>
+                                
                             </a>
                         @endif
                     @endforeach
                     <!-- ページャー -->
                     {{ $myRecipes->links() }}
                 </div>
-            </div>
         </div>
     </div>
 </x-app-layout>
