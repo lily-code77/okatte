27ee35df2616b806120a09414eae2f4353c36069
totoki <totoki.sayoko@gmail.com>
2024-06-22 21:50:31 +0900

    styled show for bookmarks

--- resources/views/bookmarks/index.blade.php
+++ resources/views/bookmarks/index.blade.php
@@ -1,29 +1,47 @@
 <x-app-layout>
 
-    <div class="container">
-    {{ __("ブックマーク") }}
-        @if($bookmarkedArticles->isEmpty())
-            <p>ブックマークした記事／レシピはありません</p>
-        @else
-            <ul class="list-group">
+    <section class="container m-8">
+        <div>
+            @if($bookmarkedArticles->isEmpty())
+                <p>ブックマークした記事／レシピはありません</p>
+            @else
+                <p class="tcl text-4xl font-extrabold">ブックマーク</p>
                 @foreach($bookmarkedArticles as $article)
-                    <li class="list-group-item">
-                        <a href="{{ route('article.show', $article->id) }}">
-                            <img class=q_img src="{{ asset('storage/' . $article->image) }}" alt="">
-                            {{ $article->title }}
+                    <div class="container m-8 bg-white rounded-lg shadow">
+                        <a class="m-1" href="{{ route('article.show', ['article' => $article->id]) }}">
+                            <div class="flex">
+                                <p class="mx-5 tcl">記事</p>
+                                <img class="w-48" src="{{ asset('storage/' . $article->image) }}" alt="">
+                                <div class="ml-5 leading-relaxed">
+                                    <p class="text-base tcl">@ {{ $article['user']['name'] }}</p>
+                                    <p class="text-xs text-slate-400">{{ $article->updated_at }}</p>
+                                    <p class="text-2xl font-bold text-pretty tcl">{{ $article->title }}</p>
+                                    <p class="text-base text-slate-500">{{ $article->tags }}</p>
+                                </div>
+                            </div>
                         </a>
-                    </li>
+                    </div>
                 @endforeach
                 @foreach($bookmarkedRecipes as $recipe)
-                    <li class="list-group-item">
-                        <a href="{{ route('recipe.show', $recipe->id) }}">
-                            <img class=q_img src="{{ asset('storage/' . $recipe->image) }}" alt="">
-                            {{ $recipe->title }}
+                    <div class="container m-8 bg-white rounded-lg shadow">
+                        <a class="m-1" href="{{ route('recipe.show', ['recipe' => $recipe->id]) }}">
+                            <div class="flex">
+                                <p class="mx-5 tcl">レシピ</p>
+                                <img class="w-48" src="{{ asset('storage/' . $recipe->image) }}" alt="">
+                                <div class="ml-5 leading-relaxed">
+                                    <p class="text-base tcl">@ {{ $recipe['user']['name'] }}</p>
+                                    <p class="text-xs text-slate-400">{{ $recipe->updated_at }}</p>
+                                    <p class="text-2xl font-bold text-pretty tcl">{{ $recipe->title }}</p>
+                                    <p class="text-base text-slate-500">{{ $recipe->tags }}</p>
+                                    <p class="text-base line-clamp-3 tcl">{{ $recipe->intro }}</p>
+                                </div>
+                            </div>
                         </a>
-                    </li>
+                    </div>
                 @endforeach
-            </ul>
-        @endif
-    </div>
+            @endif
+        </div>
+        
+    </section>
 
 </x-app-layout>
