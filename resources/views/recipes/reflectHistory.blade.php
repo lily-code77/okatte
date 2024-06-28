cd64e13c204b4ebad14bbcbcbb75315385d473f5
totoki <totoki.sayoko@gmail.com>
2024-06-28 22:43:44 +0900

    placed midmap button

--- resources/views/recipes/reflectHistory.blade.php
+++ resources/views/recipes/reflectHistory.blade.php
@@ -15,14 +15,22 @@
             <input type="text" id="intro_create" name="intro" value="{{ old('intro', $recipe['intro']) }}" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
             <label for="intro_create" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">レシピの紹介文</label>
         </div>
-        <img class="w-80" src="{{ asset('storage/'. $recipe['image']) }}" alt="">
-        <div class="m-3">
-            <label class="tcl block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="image_create">画像</label>
-            <label class="my-3 focus:outline-none bg-blue-900 text-white hover:bg-blue-300 font-medium rounded text-sm px-5 py-2.5">
-                ファイルを選択
-                <input name="image" class="hidden" id="image_create" type="file" accept='image/*'>
-            </label>
+        <div class="flex justify-evenly">
+            <div class="py-4 px-4">
+                <img class="w-80" src="{{ asset('storage/'. $recipe['image']) }}" alt="">
+                <div class="m-3">
+                    <label class="tcl block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="image_create">画像</label>
+                    <label class="my-3 focus:outline-none bg-blue-900 text-white hover:bg-blue-300 font-medium rounded text-sm px-5 py-2.5">
+                        ファイルを選択
+                        <input name="image" class="hidden" id="image_create" type="file" accept='image/*'>
+                    </label>
+                </div>
+            </div>
+            <div class="py-20 px-20">
+                <a href="{{ route('mindmap.show', ['recipe' => $recipe['id']]) }}" target="_blank"><button type="button" class="mindmap-btn rounded text-sm">Mind Map</button></a>
+            </div>
         </div>
+        
         <div class="m-3">
             <label class="tcl block my-2 text-sm font-medium">材料</label>
             <p class="block p-5 w-full rounded-lg bg-gradient-to-r from-sky-200 to-indigo-200 text-base font text-sky-700 leading-loose">
