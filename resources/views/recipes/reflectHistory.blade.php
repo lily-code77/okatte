11b58b15ca8f323e1c494526c5379ccb65ae5d3c
totoki <totoki.sayoko@gmail.com>
2024-06-28 20:47:21 +0900

    changed image button and sortable

--- resources/views/recipes/reflectHistory.blade.php
+++ resources/views/recipes/reflectHistory.blade.php
@@ -18,7 +18,10 @@
         <img class="w-80" src="{{ asset('storage/'. $recipe['image']) }}" alt="">
         <div class="m-3">
             <label class="tcl block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="image_create">画像</label>
-            <input name="image" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="image_create" type="file" accept='image/*'>
+            <label class="my-3 focus:outline-none bg-blue-900 text-white hover:bg-blue-300 font-medium rounded text-sm px-5 py-2.5">
+                ファイルを選択
+                <input name="image" class="hidden" id="image_create" type="file" accept='image/*'>
+            </label>
         </div>
         <div class="m-3">
             <label class="tcl block my-2 text-sm font-medium">材料</label>
@@ -87,30 +90,39 @@
             animation: 150,
             handle: '.handle',
             onEnd: function(evt) {
-                var items = steps.querySelectorAll('.step');
-                items.forEach(function(item, index) {
-                    item.querySelector('.step-number').innerHTML = '手順' + (index + 1);
-                });
+                updateStepNumbers();
             }
+        });
+
+        steps.addEventListener('click', function(evt) {
+            if (evt.target.classList.contains('step-delete')) {
+                evt.target.closest('.step').remove();
+                updateStepNumbers();
+            }
+        });
+
+        document.getElementById('step-add').addEventListener('click', function() {
+            let stepCount = steps.querySelectorAll('.step').length;
+            let step = document.createElement('div');
+            step.classList.add('step', 'flex', 'my-3');
+            step.innerHTML = `
+                <p class="tcl step-number ml-5 mr-3 my-6">手順${stepCount + 1}</p>
+                <img class="handle mr-3" src="{{ asset('images/index/swapVert.svg') }}" alt="順序を入れ替える">
+                <div class="relative m-3">
+                    <textarea type="text" id="step_create" name="steps[]" class="step-input block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" rows="4" cols="50" placeholder=""></textarea>
+                    <label for="step_create" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">手順を入力</label>
+                </div>
+                <img class="step-delete" src="{{ asset('images/index/delete.svg') }}" alt="削除する">
+            `;
+            steps.appendChild(step);
         });
     };
 
-    document.getElementById('steps').addEventListener('click', function(evt) {
-        if (evt.target.classList.contains('step-delete')) {
-            evt.target.closest('.step').remove();
-        }
-    });
-
-    document.getElementById('step-add').addEventListener('click', function() {
-        let stepCount = steps.querySelectorAll('.step').length;
-        let step = document.createElement('div');
-        step.classList.add('step');
-        step.innerHTML = `<p class="step-number">手順${stepCount + 1}</p>
-                        <img class="handle" src="{{ asset('images/index/swapVert.svg') }}" alt="">
-                        <input type="text" name="steps[]" placeholder="手順を入力" class="">
-                        <img class="step-delete" src="{{ asset('images/index/delete.svg') }}" alt="削除する">`;
-
-        steps.appendChild(step);
-    })
+    function updateStepNumbers() {
+        let steps = document.querySelectorAll('.step');
+        steps.forEach(function(step, index) {
+            step.querySelector('.step-number').innerHTML = '手順' + (index + 1);
+        });
+    }
 </script>
 </x-app-layout>
