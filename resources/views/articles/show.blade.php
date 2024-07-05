ee09e10b2db46eebf4b352d0f63e82c5830ad34c
totoki <totoki.sayoko@gmail.com>
2024-07-05 21:27:34 +0900

    made show files responsive

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,72 +1,79 @@
 <x-app-layout>
-<section class="container mx-3">
-<div class="flex">
-    <h3 class="pt-2 text-2xl font-bold text-pretty tcl dark:text-slate-500">【{{ $article['title'] }}】</h3>
-    <h4 class="pt-4 pl-9 text-base tcl text-right dark:text-slate-500">by {{ $article['user']['name'] }}</h4>
-</div>
-<p class="mt-5 tags text-base tcl dark:text-white">{{ $article['tags'] }}</p>
+    <section class="container mx-3">
+        <div class="flex flex-col md:flex-row items-center md:items-start md:justify-between">
+            <h3 class="pt-2 text-2xl font-bold text-pretty tcl dark:text-slate-500 md:w-3/4">{{ $article['title'] }}</h3>
+            <h4 class="pt-4 md:pt-2 text-base tcl text-right dark:text-slate-500 md:w-1/4">by {{ $article['user']['name'] }}</h4>
+        </div>
+        <p class="mt-5 tags text-base tcl dark:text-white">{{ $article['tags'] }}</p>
+        <div class="flex justify-center">
+            <img class="mt-5 max-w-80 h-auto" src="{{ asset('storage/'. $article['image']) }}" alt="{{ $article['title'] }}">
+        </div>
+        <div class="bg-white dark:bg-slate-500 mt-5 p-4 rounded">
+            <p class="text-base tcl">{!! $article['content'] !!}</p>
+        </div>
 
-<div>
-    <img class="mt-5 w-80" src="{{ asset('storage/'. $article['image']) }}" alt="{{ $article['title'] }}">
-</div>
-<div class="bg-white dark:bg-slate-500">
-    <p class="m-4 rounded text-base tcl bg-white">{!! $article['content'] !!}</p>
-</div>
+        <!-- LIKE機能 -->
+        <div class="flex justify-center mt-5">
+            @if($article->favorites->where('user_id', Auth::id())->count())
+                <form id="unfavorite-form" action="{{ route('favorites.destroy', $article) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Unfavorite</button>
+                </form>
+            @else
+                <form id="favorite-form" action="{{ route('favorites.store', $article) }}" method="POST">
+                    @csrf
+                    <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Favorite</button>
+                </form>
+            @endif
+        </div>
 
-<!-- LIKE機能 -->
-<div>
-@if($article->favorites->where('user_id', Auth::id())->count())
-    <form class="m-5" id="unfavorite-form" action="{{ route('favorites.destroy', $article) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Unfavorite</button>
-    </form>
-@else
-    <form class="m-5" id="favorite-form" action="{{ route('favorites.store', $article) }}" method="POST">
-        @csrf
-        <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Favorite</button>
-    </form>
-@endif
-</div>
+        <!-- Bookmark機能 -->
+        <div class="flex justify-center mt-5">
+            @if($article->bookmarks->where('user_id', Auth::id())->count())
+                <form action="{{ route('bookmarks.destroy', $article) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Unbookmark</button>
+                </form>
+            @else
+                <form action="{{ route('bookmarks.store', $article) }}" method="POST">
+                    @csrf
+                    <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Bookmark</button>
+                </form>
+            @endif
+        </div>
 
-<!-- Bookmark機能 -->
-<div>
-@if($article->bookmarks->where('user_id', Auth::id())->count())
-    <form class="m-5" action="{{ route('bookmarks.destroy', $article) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Unbookmark</button>
-    </form>
-@else
-    <form class="m-5" action="{{ route('bookmarks.store', $article) }}" method="POST">
-        @csrf
-        <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Bookmark</button>
-    </form>
-@endif
-</div>
+        <!-- SNS各種のシェアボタン -->
+        <div class="flex justify-center mt-5 space-x-4">
+            <div id="fb-root"></div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v20.0" nonce="BkDmKogm"></script>
+            <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small">
+                <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">シェアする</a>
+            </div>
+            <div>
+                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+            <div>
+                <a href="https://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="touch-counter" data-hatena-bookmark-width="40" data-hatena-bookmark-height="40" title="このエントリーをはてなブックマークに追加">
+                    <img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" />
+                </a>
+                <script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
+            </div>
+        </div>
+    </section>
 
-<!-- SNS各種のシェアボタン -->
-<div class="flex">
-    <div id="fb-root"></div>
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v20.0" nonce="BkDmKogm"></script>
-    <div class="m-2 fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">シェアする</a></div>
-    <div><a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="m-2 twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
-    <div><a href="https://b.hatena.ne.jp/entry/" class="m-2 hatena-bookmark-button" data-hatena-bookmark-layout="touch-counter" data-hatena-bookmark-width="40" data-hatena-bookmark-height="40" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script></div>
-</div>
-
-</section>
-
-
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        document.querySelectorAll('form').forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!@json(Auth::check())) {
-                    event.preventDefault();
-                    window.location.href = '{{ route('login') }}';
-                }
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            document.querySelectorAll('form').forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!@json(Auth::check())) {
+                        event.preventDefault();
+                        window.location.href = '{{ route('login') }}';
+                    }
+                });
             });
         });
-    });
-</script>
+    </script>
 </x-app-layout>
--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,142 +1,147 @@
 <x-app-layout>
-<section class="container mx-3">
+    <section class="container mx-auto px-4 sm:px-6 lg:px-8">
 
-<div class="flex">
-    <h3 class="pt-2 text-2xl font-bold text-pretty tcl dark:text-slate-500">【{{ $recipe['title'] }}】</h3>
-    <h4 class="pt-4 pl-9 text-base tcl dark:text-slate-500 text-right">by {{ $recipe['user']['name'] }}</h4>
-</div>
-<p class="mt-5 tags text-base dark:text-white">{{ $recipe['tags'] }}</p>
-
-<div class="flex m-5">
-    <img class="w-80" src="{{ asset('storage/'. $recipe['image']) }}" alt="{{ $recipe['title'] }}">
-    <p class="m-20 tcl text-base my-3 dark:text-slate-500">レシピ背景などのエピソード：<br>{{ $recipe['comment'] }}</p>
-</div>
-<div>
-    <p class="tcl text-base my-3 dark:text-slate-500">材料：</p>
-    <p class="ing m-4 rounded text-base tcl bg-white dark:bg-slate-400"></p>
-</div>
-<div class="bg-slate-300 dark:bg-slate-400 rounded py-3">
-
-<?php
-    $lastSteps = [];
-    $lastStepCreatedAt = $recipe['steps'][count($recipe['steps']) - 1]['created_at'];
-?>
-
-@foreach($recipe['steps'] as $s)
-    @if ($s['created_at'] == $lastStepCreatedAt)
-        @php
-            $lastSteps[] = $s;
-        @endphp
-        <div class="">
-            <div class="">
-            <p class="m-4 rounded text-base tcl dark:text-gray-700">手順{{ $s['step_number'] }}:</p>
-            </div>
-            <p class="m-4 rounded text-base tcl bg-white dark:text-white dark:bg-gray-700 dark:border-gray-600">{!! nl2br($s['description']) !!}</p>
+        <div class="flex flex-col md:flex-row justify-between items-start md:items-center mt-5">
+            <h3 class="text-2xl font-bold text-pretty tcl dark:text-slate-500">【{{ $recipe['title'] }}】</h3>
+            <h4 class="text-base tcl dark:text-slate-500 text-right mt-4 md:mt-0 md:pl-9">by {{ $recipe['user']['name'] }}</h4>
         </div>
-    @endif
-@endforeach
 
-<?php $json = json_encode($lastSteps); ?>
-</div>
+        <p class="mt-5 tags text-base dark:text-white">{{ $recipe['tags'] }}</p>
 
-<!-- LIKE機能 -->
-<div>
-@if($recipe->favorites->where('user_id', Auth::id())->count())
-    <form class="m-5" action="{{ route('favorites.recipeDestroy', $recipe) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Unfavorite</button>
-    </form>
-@else
-    <form class="m-5" action="{{ route('favorites.recipeStore', $recipe) }}" method="POST">
-        @csrf
-        <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Favorite</button>
-    </form>
-@endif
-</div>
+        <div class="flex flex-col md:flex-row items-start mt-5">
+            <img class="w-full md:w-1/3 max-w-full h-auto" src="{{ asset('storage/'. $recipe['image']) }}" alt="{{ $recipe['title'] }}">
+            <p class="mt-5 md:mt-0 md:ml-20 tcl text-base dark:text-slate-500">レシピ背景などのエピソード：<br>{{ $recipe['comment'] }}</p>
+        </div>
 
-<!-- Bookmark機能 -->
-<div>
-@if($recipe->bookmarks->where('user_id', Auth::id())->count())
-    <form class="m-5" action="{{ route('bookmarks.recipeDestroy', $recipe) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Unbookmark</button>
-    </form>
-@else
-    <form class="m-5" action="{{ route('bookmarks.recipeStore', $recipe) }}" method="POST">
-        @csrf
-        <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Bookmark</button>
-    </form>
-@endif
-</div>
+        <div class="mt-5">
+            <p class="tcl text-base dark:text-slate-500">材料：</p>
+            <p class="ing mt-4 p-4 rounded text-base tcl bg-white dark:bg-slate-400"></p>
+        </div>
 
-<!-- SNS各種のシェアボタン -->
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v20.0" nonce="BkDmKogm"></script>
-<div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">シェアする</a></div>
-<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-<a href="https://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="touch-counter" data-hatena-bookmark-width="40" data-hatena-bookmark-height="40" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
+        <div class="bg-slate-300 dark:bg-slate-400 rounded py-3 mt-5">
+            <?php
+                $lastSteps = [];
+                $lastStepCreatedAt = $recipe['steps'][count($recipe['steps']) - 1]['created_at'];
+            ?>
 
-</section>
+            @foreach($recipe['steps'] as $s)
+                @if ($s['created_at'] == $lastStepCreatedAt)
+                    @php
+                        $lastSteps[] = $s;
+                    @endphp
+                    <div class="mt-4">
+                        <p class="m-4 rounded text-base tcl dark:text-gray-700">手順{{ $s['step_number'] }}:</p>
+                        <p class="m-4 rounded text-base tcl bg-white dark:text-white dark:bg-gray-700 dark:border-gray-600">{!! nl2br($s['description']) !!}</p>
+                    </div>
+                @endif
+            @endforeach
 
-<script>
-    // 材料の抽出関数
-function parseStep(step) {
-    const ingredientPattern = /@(.+?)\{(.*?)\}/g;
-    let match;
-    const results = [];
+            <?php $json = json_encode($lastSteps); ?>
+        </div>
 
-    while ((match = ingredientPattern.exec(step)) !== null) {
-        const ingredient = match[1];
-        const quantity = match[2];
+        <!-- LIKE機能 -->
+        <div class="mt-5">
+            @if($recipe->favorites->where('user_id', Auth::id())->count())
+                <form class="flex justify-center" action="{{ route('favorites.recipeDestroy', $recipe) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Unfavorite</button>
+                </form>
+            @else
+                <form class="flex justify-center" action="{{ route('favorites.recipeStore', $recipe) }}" method="POST">
+                    @csrf
+                    <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Favorite</button>
+                </form>
+            @endif
+        </div>
 
-        results.push({
-            ing: ingredient,
-            quantity: quantity
-        });
-    }
+        <!-- Bookmark機能 -->
+        <div class="mt-5">
+            @if($recipe->bookmarks->where('user_id', Auth::id())->count())
+                <form class="flex justify-center" action="{{ route('bookmarks.recipeDestroy', $recipe) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Unbookmark</button>
+                </form>
+            @else
+                <form class="flex justify-center" action="{{ route('bookmarks.recipeStore', $recipe) }}" method="POST">
+                    @csrf
+                    <button type="submit" class="focus:outline-none button dark:bg-gray-500 dark:text-white font-medium rounded text-sm px-5 py-2.5">Bookmark</button>
+                </form>
+            @endif
+        </div>
 
-    return results;
-}
+        <!-- SNS各種のシェアボタン -->
+        <div id="fb-root" class="mt-5 flex justify-center space-x-4"></div>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v20.0" nonce="BkDmKogm"></script>
+        <div class="flex justify-center space-x-4 mt-5">
+            <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small">
+                <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">シェアする</a>
+            </div>
+            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <a href="https://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="touch-counter" data-hatena-bookmark-width="40" data-hatena-bookmark-height="40" title="このエントリーをはてなブックマークに追加">
+                <img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" />
+            </a>
+            <script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
+        </div>
 
-let jsonData = `<?=$json?>`;
-jsonData = jsonData.replace(/[\u0000-\u001F\u007F-\u009F]/g, "");// JSON文字列内の制御文字を除去する
-jsonData = jsonData.replace(/\r\n/g, "\n"); // 改行文字の置換
-console.log({jsonData});
-jsonData = JSON.parse(jsonData);
+    </section>
 
-for (const element of jsonData) {
-    let step = parseStep(element.description);
-    console.log(step);
+    <script>
+        // 材料の抽出関数
+        function parseStep(step) {
+            const ingredientPattern = /@(.+?)\{(.*?)\}/g;
+            let match;
+            const results = [];
 
-    // 既存の <p class="ing"></p> を取得
-    let pElement = document.querySelector('.ing');
+            while ((match = ingredientPattern.exec(step)) !== null) {
+                const ingredient = match[1];
+                const quantity = match[2];
 
-    // 取得した <p> 要素に step を追加
-    if (pElement) {
-        for (const e of step) {
-            pElement.innerHTML += e.ing + ":" + e.quantity + '<br>';
+                results.push({
+                    ing: ingredient,
+                    quantity: quantity
+                });
+            }
+
+            return results;
         }
-    } else {
-        console.error('No element with class "ing" found.');
-    }
-}
 
+        let jsonData = `<?=$json?>`;
+        jsonData = jsonData.replace(/[\u0000-\u001F\u007F-\u009F]/g, ""); // JSON文字列内の制御文字を除去する
+        jsonData = jsonData.replace(/\r\n/g, "\n"); // 改行文字の置換
+        console.log({jsonData});
+        jsonData = JSON.parse(jsonData);
 
-    //
-    document.addEventListener('DOMContentLoaded', function () {
-        document.querySelectorAll('form').forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!@json(Auth::check())) {
-                    event.preventDefault();
-                    window.location.href = '{{ route('login') }}';
+        for (const element of jsonData) {
+            let step = parseStep(element.description);
+            console.log(step);
+
+            // 既存の <p class="ing"></p> を取得
+            let pElement = document.querySelector('.ing');
+
+            // 取得した <p> 要素に step を追加
+            if (pElement) {
+                for (const e of step) {
+                    pElement.innerHTML += e.ing + ":" + e.quantity + '<br>';
                 }
+            } else {
+                console.error('No element with class "ing" found.');
+            }
+        }
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.querySelectorAll('form').forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!@json(Auth::check())) {
+                        event.preventDefault();
+                        window.location.href = '{{ route('login') }}';
+                    }
+                });
             });
         });
-    });
 
-    
-</script>
+    </script>
 
 </x-app-layout>
